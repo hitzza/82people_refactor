0414f0ba92a8f7283570e43c1edb1c5e2f0a1bec
hitzza <cslzh4433@google.com>
2024-12-11 22:43:40 +0900

    creare db

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models/db')
 
 // 기본 라우트
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'api start!' });
 });
+async function getUsers() {
+  const [rows] = await db.query('SELECT * FROM test');
+  console.log(rows);
+}
+
+getUsers();
 
 module.exports = router;
